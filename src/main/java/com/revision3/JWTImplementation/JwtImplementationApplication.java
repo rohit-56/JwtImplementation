be283af13b1136bdb5810f3ddb8373399b020662
Rohit <rohit.sharma2@hexaviewtech.com>
2022-04-04 14:01:51 +0530

    Commiting all the src files

---
+++ src/main/java/com/revision3/JWTImplementation/JwtImplementationApplication.java
@@ -0,0 +1,15 @@
+package com.revision3.JWTImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class JwtImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtImplementationApplication.class, args);
+	}
+
+}
